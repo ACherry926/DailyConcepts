3e0cb231b9dc90fac7539771d77b1edf6e978727
ACherry926 <92986614+ACherry926@users.noreply.github.com>
2025-08-04 19:09:44 +0530

    Revert "Modify the data"
    This reverts commit a93bc34068267fa3f3e65eae6322acfe22ed22f8.

--- DailyConcepts/Day28Concepts/Repository/AccountRepository.cs
+++ DailyConcepts/Day28Concepts/Repository/AccountRepository.cs
@@ -1,12 +1,6 @@
-﻿using Day28Concepts.Model;
+﻿using AutoMapper.Configuration;
+using Day28Concepts.Model;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Day28Concepts.Repository
@@ -41,31 +35,31 @@ namespace Day28Concepts.Repository
             return await _userManager.CreateAsync(user,studentModel.Password);
         }
 
-        public async Task<string> LoginAsync(SignInModel signInModel)
-        {
-            var result = await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, false, false);
+        //public async Task<string> LoginAsync(SignInModel signInModel)
+        //{
+        //    var result = await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, false, false);
+            
+        //    if(!result.Succeeded)
+        //    {
+        //        return null;
+        //    }
 
-            if (!result.Succeeded)
-            {
-                return null;
-            }
+        //    var authClaims = new List<Claim>
+        //    {
+        //        new Claim (ClaimTypes.Name, signInModel.Email),
+        //        new Claim (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        //    };
+        //    var authSigninkey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]));
 
-            var authClaims = new List<Claim>
-            {
-                new Claim (ClaimTypes.Name, signInModel.Email),
-                new Claim (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
-            var authSigninkey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]));
+        //    var token = new JwtSecurityToken(
+        //        issuer: _configuration["Jwt:ValidIssuer"],
+        //        audience: _configuration["Jwt:ValidAudience"],
+        //        expires: DateTime.Now.AddDays(1),
+        //        claims: authClaims,
+        //        signingCredientials: new SigningCredentials(authSigninkey, SecurityAlgorithms.HmacSha256Signature)
+        //        );
 
-            var token = new JwtSecurityToken(
-                issuer: _configuration["Jwt:ValidIssuer"],
-                audience: _configuration["Jwt:ValidAudience"],
-                expires: DateTime.Now.AddDays(1),
-                claims: authClaims,
-                signingCredentials: new SigningCredentials(authSigninkey, SecurityAlgorithms.HmacSha256Signature)
-                );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
     }
 }
